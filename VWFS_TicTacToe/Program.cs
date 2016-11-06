588749bf9b2d52d31faa667a5b25a666303a6807
Daniel <ddannewitz@web.de>
2016-11-06 09:06:31 +0100

    Add project files.

---
+++ VWFS_TicTacToe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VWFS_TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
